96ed5a7efa2ff31957b93195501fbe0ead0c4344
Naoto <naototomita930@gmail.com>
2026-07-02 00:09:09 +0900

    0702_01

--- lib/link_screen_folder/link_screen.dart
+++ lib/link_screen_folder/link_screen.dart
@@ -10,8 +10,10 @@ class LinkScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF5F7FB),
       appBar: AppBar(
-        title: const Text('学習仲間'),
+        backgroundColor: Colors.white,
+        title: const Text('Link'),
         centerTitle: true,
       ),
       body: Padding(
--- lib/link_screen_folder/qanda_screen.dart
+++ lib/link_screen_folder/qanda_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+git
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
