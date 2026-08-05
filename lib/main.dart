174580a3bb3ac84ac52416f1a29f32a789ac91e4
hakusan23 <hakucha88@gmail.com>
2026-08-05 23:38:29 +0900

    data

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'mypage_screen_folder/mypage_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+import 'splash_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
---
+++ lib/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:study_support_app/data_screen.dart';
+import 'package:study_support_app/home_screen_folder/home_screen.dart';
+
+import 'home_screen_folder/home_screen.dart';
+import 'data_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    checkUser();
+  }
+
+  Future<void> checkUser() async {
+
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if(user == null){
+      final credential =
+      await FirebaseAuth.instance.signInAnonymously();
+
+      user = credential.user;
+    }
+
+    final doc = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get();
+
+    if(!mounted) return;
+
+    if(doc.exists){
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (_) => const HomeScreen()));
+    }else{
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (_) => const OnboardingScreen()));
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
