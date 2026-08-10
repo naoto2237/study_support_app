7c4e04460b159f415157dd851aeffa9dfbbdb9f4
Naoto <naototomita930@gmail.com>
2026-08-10 21:53:27 +0900

    0810_03

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ import 'mypage_screen_folder/mypage_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,9 +31,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Study Support App',
+
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+
+      supportedLocales: const [Locale('ja')],
       theme: ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.deepPurple,
+        colorSchemeSeed: Color(0xFFF7F7F7),
         textTheme: GoogleFonts.notoSansJpTextTheme(),
 
         textSelectionTheme: const TextSelectionThemeData(
@@ -39,7 +50,7 @@ class MyApp extends StatelessWidget {
           selectionHandleColor: Color(0xFF2196F3), // ハンドル
         ),
       ),
-      home: const MainNavigationScreen(),
+      home: const SplashScreen(),
     );
   }
 }
@@ -97,7 +108,6 @@ class _MainNavigationScreenState extends State<MainNavigationScreen>
       }
     });
   }
-
 
   @override
   void dispose() {
